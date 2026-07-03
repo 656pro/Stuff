db1a00bf776de8353e0adbf63a1d1057b8e2f830
X <109253617+981726@users.noreply.github.com>
2026-07-03 00:23:53 -0700

    Refactor game loading check for clarity

--- redeem.lua
+++ redeem.lua
@@ -1,5 +1,8 @@
 repeat task.wait(0.25) until game:GetService("Players") and game:GetService("Players").LocalPlayer
-if not game:IsLoaded() then game.Loaded:Wait() task.wait(3) end
+if not game:IsLoaded() then 
+    game.Loaded:Wait() 
+    task.wait(3) 
+end
 
 for _, code in ipairs(RD.Redeem) do
     game:GetService('ReplicatedStorage').MainEvent:FireServer('EnterPromoCode', code)
